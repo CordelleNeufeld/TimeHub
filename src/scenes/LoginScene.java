ec9dfd3b492fe8908bf83d4ba12dd2c637c38fcd
Cordelle Neufeld <cordelle.neufeld01@stclairconnect.ca>
2019-11-29 17:00:24 -0500

    #40 Added basic style to GUI * added resources package * initial style experimentation with LoginPane and LoginScene

--- src/scenes/LoginScene.java
+++ src/scenes/LoginScene.java
@@ -7,6 +7,11 @@ public class LoginScene extends Scene {
 
 	public LoginScene() {
 		super(new LoginPane(), 1000, 1000);
+		
+		String path = this.getClass().getResource("/resources/stylesheet.css").toExternalForm();
+		this.getStylesheets().add(path);
+		
+		
 	}
 
 }
