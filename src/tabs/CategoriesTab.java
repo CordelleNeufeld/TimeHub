e46dc4540c5c29cc8915211c9626df90cf049ce9
AbelAnderson <Asatheabel@gmail.com>
2019-12-01 15:19:11 -0500

    Updated Categories and Projects

--- src/tabs/CategoriesTab.java
+++ src/tabs/CategoriesTab.java
@@ -1,8 +1,12 @@
 package tabs;
 
 import javabeans.Category;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import panes.TabsPane;
 import tables.CategoriesTable;
 
 import java.util.ArrayList;
@@ -14,9 +18,13 @@ public class CategoriesTab extends Tab {
     private CategoriesTab() {
     	ArrayList<Category> categories = new CategoriesTable().getAllCategories();
 
+        Text title = new Text("Categories");
+        title.setTextAlignment(TextAlignment.CENTER);
+
         //Set up the List View
         Accordion accordion = new Accordion();
-        accordion.setMaxHeight(200);
+        accordion.setMaxHeight(300);
+        accordion.setMaxWidth(500);
 
         for (Category category : categories) {
             TitledPane titledPane = new TitledPane(category.getTitle(), new Label(category.getDescription()));
@@ -32,10 +40,11 @@ public class CategoriesTab extends Tab {
             TabsPane.tabPane.getSelectionModel().select(formTab);
         });
 
-        //Set up borderPane
-        BorderPane mainPane = new BorderPane();
-        mainPane.setCenter(accordion);
-        mainPane.setBottom(newCategoryButton);
+        //Set up VBox
+        VBox mainPane = new VBox();
+        mainPane.getChildren().addAll(title, newCategoryButton, accordion);
+        mainPane.setSpacing(100);
+        mainPane.setAlignment(Pos.CENTER);
 
         //Add content to Tab
         setContent(mainPane);
